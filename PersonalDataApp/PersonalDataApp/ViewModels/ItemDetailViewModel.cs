7f44d2105adde50d6766affded75af4e10d63faa
William Sandvej Hansen <grusinator@gmail.com>
2018-07-28 12:36:20 +0200

    Add project files.

---
+++ PersonalDataApp/PersonalDataApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using PersonalDataApp.Models;
+
+namespace PersonalDataApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
